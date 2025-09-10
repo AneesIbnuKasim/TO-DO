1a5bb7d52011f1d12009b5d83489880048a60c3d
Anees <Aneesdropz@gmail.com>
2025-09-10 14:56:25 +0530

    Completed home component ui and added functionality to add todos on states and display it on the ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './home.css'
 
 function Home() {
+
+    const [input, setInput] = useState('')
+    const [todos, setTodos] = useState([])
+
+    //handle todo sumbmission 
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log('input',input);
+        const todo = input.trim()
+        if(todo) {
+          setTodos(prev=>[...prev,todo])
+          setInput('')
+        }
+        
+    }
+
+    // useEffect(()=>{
+    //   console.log('todo',todos);
+      
+    // },[todos])
+
   return (
-    <div className='container text-primary'>
-      <h1>To Do App</h1>
-      <div></div>
+    <div className='container text-primary d-flex justify-content-center align-items-center vh-100 w-100'>
+     <div className='bg-success text-white rounded-3 p-5 w-50 text-center d-flex flex-column align-items-center'>
+     <h1 className='display-3 border-bottom-1 border-dark mb-4'>To Do App</h1>
+      <div className='border-1 border-black w-100'>
+            <form action="" className=''>
+              <div className="d-flex gap-3 w-100 mb-5">
+                <input value={input} onChange={(e)=>setInput(e.target.value)} type="text" className='w-100 rounded-3 p-1 fs-4' 
+                placeholder='type here...'/>
+              <button className='btn btn-primary ' onClick={handleSubmit}>Submit</button>
+              </div>
+            </form>
+            <div className=' mt-3'>
+              <ul className='list-unstyled d-flex flex-column gap-3'>
+                {
+                  todos.map((todo,index)=>(
+                  <li key={index} className='bg-dark p-3 rounded-2 col-12'>
+                    {todo}
+                  </li>)
+                  )
+                }
+              </ul>
+          </div>
+      </div>
+     </div>
     </div>
   )
 }
