b3c43caaae76083b1ebd798924a4b94d29a161c8
Anees <Aneesdropz@gmail.com>
2025-09-10 11:39:24 +0530

    installed react-router-dom, bootstrap and checked if bootstrap is working also added home page for the app with a css file fore it

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container text-primary'>
+      <h1>To Do App</h1>
+      <div></div>
+    </div>
+  )
+}
+
+export default Home
